f6fc6907988cbe78cf54c6e11eb40dc0a338f672
TrentDev <TrentLathim@gmail.com>
2022-01-09 23:19:56 -0800

    added comments to increase code clarity

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
+// declaring variables related to quiz timer
 let timeLeft;
 let timerId;
 
+// logic that makes the timer count down every second
 function countDown() {
-  if (timeLeft === 0) clearInterval(timerId);
-  else timeLeft--;
+  if (timeLeft === 0) {
+    document.querySelector("#startQuiz").innerText = "Restart Quiz!";
+    clearInterval(timerId);
+  } else timeLeft--;
   document.querySelector("#timeLeft").innerText = timeLeft;
 }
 
+// onclick function that starts timer
 function startTimer() {
-  if (timeLeft <= 200) alert("The quiz has already been started");
+  if (timeLeft === 0) {
+    timeLeft = 20;
+    timerId = setInterval(countDown, 1000);
+  } else if (timeLeft < 20) alert("The quiz has already been started");
   else {
-    timeLeft = 200;
+    timeLeft = 20;
     timerId = setInterval(countDown, 1000);
   }
 }
